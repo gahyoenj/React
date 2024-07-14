3cc1b9a8b509e1f8d2d74bdf3fc9f34cde8a850d
gahyoenj <rkgus678@gmail.com>
2024-07-14 20:52:10 +0900

    update: 공통 컴포넌트 구현

--- onebite-react/section12/src/App.jsx
+++ onebite-react/section12/src/App.jsx
@@ -5,6 +5,10 @@ import Home from './pages/Home';
 import Diary from './pages/Diary';
 import New from './pages/New';
 import NotFound from './pages/NotFound';
+import Button from './components/Button';
+import Header from './components/Header';
+
+import { getEmotionImage } from './util/get-emotion-image';
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -17,12 +21,40 @@ function App() {
   };
   return (
     <>
+      <Header title={'Header'}
+        letfChild={<Button text={'Left'}/>}
+        rightChild={<Button text={'Right'}/>}/>
+      <Button 
+        text={'123'}
+        type={'DEFAULT'}
+        onClick={() =>{
+          console.log('123번 버튼 클릭');
+      }}/>
+      <Button 
+        text={'123'}
+        type={'POSITIVE'}
+        onClick={() =>{
+          console.log('123번 버튼 클릭');
+      }}/>
+      <Button 
+        text={'123'}
+        type={'NEGATIVE'}
+        onClick={() =>{
+          console.log('123번 버튼 클릭');
+      }}/>
+      {/* <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
         <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      </div> */}
+      {/* <button onClick={onClickButton}>New 페이지로 이동</button> */}
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/new" element={<New/>}/>
