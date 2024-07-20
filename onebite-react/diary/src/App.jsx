36341ecc4f79fcdf2a0fb0a0352dbe29b860d389
gahyoenj <rkgus678@gmail.com>
2024-07-20 22:03:48 +0900

    update: 일기장 home 페이지 기능,ui 구현

---
+++ onebite-react/diary/src/App.jsx
@@ -0,0 +1,128 @@
+import { useState,createContext } from 'react'
+import { Route, Routes,Link, useNavigate } from 'react-router-dom'
+import './App.css'
+import { useReducer,useRef } from 'react';
+import Home from './pages/Home';
+import Diary from './pages/Diary';
+import New from './pages/New';
+import Edit from "./pages/Edit";
+import NotFound from './pages/NotFound';
+import Button from './components/Button';
+import Header from './components/Header';
+
+import { getEmotionImage } from './util/get-emotion-image';
+
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 작성하는 New 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+
+const mockData = [
+  {
+    id:1,
+    createdDate: new Date("2024-07-20").getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id:2,
+    createdDate: new Date("2024-07-19").getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+  {
+    id:3,
+    createdDate: new Date("2024-06-24").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
+  }
+];
+
+function reducer(state, action) {
+  switch(action.type){
+    case "CREATE" : 
+      return [action.data,...state];
+    case "UPDATE" :
+      return state.map((item)=>String(item.id) === String(action.data.id) ? action.data : item);
+    case "DELETE" :
+      return state.filter((item)=>String(item.id) !== String(action.id));
+    default:
+      return state
+  }
+}
+
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
+
+
+function App() {
+
+  const [data, dispatch] = useReducer(reducer, mockData);
+
+  const idRef = useRef(3);
+  // 새로운 일기 추가
+  const onCreate = (createdData, emotionId, content) => {
+    // 새로운 일기를 추가하는 기능
+    dispatch({
+      type:"CREATE",
+      data : {
+        id: idRef.current++,
+        createdData,
+        emotionId,
+        content,
+      },
+    });
+
+  };
+
+  // 기존 일기 수정
+  const onUpdate = (id, createdData, emotionId, content) => {
+    // 기존 일기 수정하는 기능
+    dispatch(
+      {
+        type:"UPDATE",
+        data:{
+          id,
+          createdData,
+          emotionId,
+          content
+        }
+      }
+    );
+  }
+
+  // 일기 삭제
+  const onDelete = (id) => {
+    dispatch(
+      {
+        type:"DELETE",
+        id,
+      }
+    );
+  }
+
+
+  return (
+    <>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{
+        onCreate,
+        onUpdate,
+        onDelete,
+      }}>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/new" element={<New/>}/>
+        <Route path="/diary/:id" element={<Diary/>}/>
+        <Route path="/edit/:id" element={<Edit/>} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
+    </>
+  );
+}
+
+export default App;
+
+// Routes 컴포넌트 안에는 Route 컴포넌트들만 들어올 수 있음
+// Routes 컴포넌트 밖에 정의된 것들은 페이지 렌더링과 관계없이 모든 페에지에서 다 렌더링 됨
---
+++ onebite-react/diary/src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
+import { useState,useContext } from "react";
+import { DiaryStateContext } from "../App";
+
+
+const getMonthlyData = (pivotDate, data) =>{
+    const beginTime = new Date(
+        pivotDate.getFullYear(), 
+        pivotDate.getMonth(), 
+        1, 0, 0, 0).getTime(); //pivotDate의 연도와 해당하는 월의 1일 0시 0분 0초 해당하는 값을 숫자형태로
+    
+    const endTime = new Date(
+        pivotDate.getFullYear(),
+        pivotDate.getMonth()+1,
+        0 ,23, 59, 59
+    ).getTime(); //pivotDate 달+1의 이전 달의 마지막 날 23시 59분 59초
+    return data.filter((item)=> beginTime <= item.createdDate && item.createdDate <= endTime);
+}
+
+const Home = () => {
+    const data = useContext(DiaryStateContext);
+
+    const [pivotDate, setPivotDate] = useState(new Date());
+
+    const monthlyData = getMonthlyData(pivotDate, data);
+    console.log(monthlyData);
+
+    const onIncreaseMonth = () =>{
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth()+1));
+    }
+    const onDecreaseMonth = () =>{
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth()-1));
+    }
+
+    return (
+    <div>
+        <Header title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth()+1}월`}
+            letfChild={<Button onClick={onDecreaseMonth} text={"<"}/>}
+            rightChild={<Button onClick={onIncreaseMonth} text={">"}/>}/>
+        <DiaryList data={monthlyData}/>
+        </div>);
+};
+
+export default Home;
